050ace81036b2b32c6dfd3ec497a62d5bfd1211a
KABIR ANWAR <anwarkabir2011@gmail.com>
2022-01-28 09:44:20 -0500

    added aws codepipeline

--- bin/aws-ecs-cluster.ts
+++ bin/aws-ecs-cluster.ts
@@ -8,25 +8,33 @@ import { ScoutDBStack } from '../lib/scout-db';
 import { Stack, Tags } from '@aws-cdk/core';
 //import { Budget } from '../lib/budget';
 import * as parameters from "../cdk.json"
+import { cdkpipelinestack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
 const scoutFrontEndStack = new Stack(app, 'scoutFrontEndStack');
 Tags.of(scoutFrontEndStack).add('env', 'dev');
-const appstack = parameters.context.config.ProjectName + "-APP-stack" + parameters.context.config.ProjectEnvironment ;
+const appstack = parameters.context.config.ProjectName + "-APP-stack" + parameters.context.config.ProjectEnvironment;
 const dbstack = parameters.context.config.ProjectName + "-DB-stack" + parameters.context.config.ProjectEnvironment;
 const myaws = { account: '321325872726', region: 'us-east-1' };
-new ScoutFrontEnd(app, 'Scout-APP-stack', { 
-    env: myaws, 
+new ScoutFrontEnd(app, 'Scout-APP-stack', {
+    env: myaws,
     tags: { 'project': 'app-scout' },
     stackName: appstack,
     description: "Stack to create application"
 });
-new ScoutDBStack(app, 'Scout-DB-stack', 
-{ 
-    env: myaws, 
-    tags: { 'project': 'app-scout' }, 
-    stackName:dbstack, 
-    description:"Stack to create db"
+new ScoutDBStack(app, 'Scout-DB-stack',
+    {
+        env: myaws,
+        tags: { 'project': 'app-scout' },
+        stackName: dbstack,
+        description: "Stack to create db"
+    });
+
+new cdkpipelinestack(app, 'cdk-pipeline-stack', {
+    env: myaws,
+    tags: { 'project': 'app-scout' },
+    stackName: "cdk-pipeline",
+    description: "Stack to create cicd"
 });
 //new ScoutDBStack(app, 'Scout-B-ECS-DB-stack', { env: myaws, tags: { 'project': 'app-scout' }, });
 
